92c1daf0e54d796ecf491dd5971cb9b36036da6f
rehana7 <89412687+rehana7@users.noreply.github.com>
2021-11-03 15:02:37 -0500

    added files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/nuts');
+var nutsRouter = require('./routes/nuts');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', nutsRouter);
+app.use('/nuts', nutsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
